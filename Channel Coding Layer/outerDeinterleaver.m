230e8ad69208380272cdb9af7bbf2ebfc4150421
mohammad khajezadeh <mhmdkh1992@gmail.com>
2025-02-07 15:02:42 -0500

    Update outerDeinterleaver.m

--- Channel Coding Layer/outerDeinterleaver.m
+++ Channel Coding Layer/outerDeinterleaver.m
@@ -9,6 +9,10 @@ elseif ~isempty(Deflag)
     storage=[storage;toOuterInterleaver];
 end
 L=floor(length(storage)/(204*8));
+toRSdec=[];
+if L == 0
+    return
+end
 todec = storage(1:L*204*8);
 storage = storage((L*204*8+1):end);
 todec=reshape(todec,8,length(todec)/8);
@@ -16,7 +20,6 @@ dec = bi2de(todec','left-msb');
 if isempty(deinterleaver)
 deinterleaver = comm.ConvolutionalDeinterleaver('NumRegisters',12,'RegisterLengthStep',17);
 end
-toRSdec=[];
 for i=1:L
     ind = ((i-1)*204+1):i*204;
     toRSdec = [toRSdec;deinterleaver(dec(ind))];
